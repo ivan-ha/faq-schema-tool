12384cbbcaa503c826de3fbaca67c837499b8122
Ivan Ha <hangolam@gmail.com>
2022-02-23 22:46:12 +0800

    feat: init FaqCard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { Layout } from "antd";
-const { Content } = Layout;
+import FaqCard from "./Components/FaqCard";
+
+const faq = [...Array(10).keys()];
 
 const App = () => {
   return (
     <Layout>
-      <h1>FAQ Schema Tool</h1>
-      <Content></Content>
+      <h1>FAQ Schema 生成器</h1>
+      {faq.map((f, index) => (
+        <FaqCard titlePlaceholder={`問題 ${index + 1}`} />
+      ))}
     </Layout>
   );
 };
---
+++ src/Components/FaqCard/index.js
@@ -0,0 +1,29 @@
+import { Card, Input } from "antd";
+import RichTextEditor from "react-rte";
+import { useState } from "react";
+
+const toolbarConfig = {
+  display: ["LINK_BUTTONS"],
+};
+
+const FaqCard = ({ titlePlaceholder }) => {
+  const title = <Input placeholder={titlePlaceholder} />;
+  const [value, setValue] = useState(RichTextEditor.createEmptyValue());
+
+  const handleChange = (v) => {
+    console.log(v.toString("html"));
+    setValue(v);
+  };
+
+  return (
+    <Card title={title}>
+      <RichTextEditor
+        value={value}
+        onChange={handleChange}
+        toolbarConfig={toolbarConfig}
+      />
+    </Card>
+  );
+};
+
+export default FaqCard;
