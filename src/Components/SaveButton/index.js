434ff194ea5fdb960f0d82599fa5e8929d9eb209
Ivan Ha <hangolam@gmail.com>
2022-02-26 16:33:25 +0800

    feat: add feedback on SaveButton

--- src/Components/SaveButton/index.js
+++ src/Components/SaveButton/index.js
@@ -1,10 +1,27 @@
 import { Button } from "antd";
 import { SaveOutlined } from "@ant-design/icons";
+import { useState } from "react";
 
-const SaveButton = ({ onClick }) => (
-  <Button type="primary" icon={<SaveOutlined />} onClick={onClick} block>
-    儲存
-  </Button>
-);
+const SaveButton = ({ onClick }) => {
+  const [saved, setSaved] = useState(false);
+
+  const handleClick = () => {
+    setSaved(true);
+    onClick();
+    setTimeout(() => setSaved(false), 1000);
+  };
+
+  return (
+    <Button
+      type="primary"
+      icon={<SaveOutlined />}
+      onClick={handleClick}
+      block
+      loading={saved}
+    >
+      {saved ? "儲存成功" : "儲存"}
+    </Button>
+  );
+};
 
 export default SaveButton;
